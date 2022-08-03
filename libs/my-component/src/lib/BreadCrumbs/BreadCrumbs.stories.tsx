872312a32525484794128ebb2feaed6df8f6b088
phamhaduy95 <ph.duy113@gmail.com>
2022-08-03 15:44:16 +0700

    add default args for BreadCrumbs story

--- libs/my-component/src/lib/BreadCrumbs/BreadCrumbs.stories.tsx
+++ libs/my-component/src/lib/BreadCrumbs/BreadCrumbs.stories.tsx
@@ -6,9 +6,24 @@ export default {
   title: 'BreadCrumbs',
 } as ComponentMeta<typeof BreadCrumbs>;
 
+
+const items = ["Home","About","Product","Author"]
+const children = items.map((item,i)=>{
+  return <BreadCrumbs.Item index={i}>
+          <a href="#">{item}</a>
+          </BreadCrumbs.Item>
+})
+
+
+
 const Template: ComponentStory<typeof BreadCrumbs> = (args) => (
-  <BreadCrumbs {...args} />
+  <BreadCrumbs {...args}>{args.children}</BreadCrumbs>
 );
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  maxItem: 2,
+  separator: '/',
+  children: children,
+  onChange:()=>{console.log("onchange")}
+};
