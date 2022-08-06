d15c81bdb55b875f734e1500bdc619708a36ad52
phamhaduy95 <ph.duy113@gmail.com>
2022-08-06 13:45:40 +0700

    initiate SideBar component in my-component

---
+++ libs/my-component/src/lib/SideBar/SideBar.tsx
@@ -0,0 +1,17 @@
+import './SideBar.scss';
+
+/* eslint-disable-next-line */
+export interface SideBarProps {
+
+
+}
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <div >
+      <h1>Welcome to SideBar!</h1>
+    </div>
+  );
+}
+
+export default SideBar;
---
+++ libs/my-component/src/lib/SideBar/SideBarItemList.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SideBarItemList() {
+  return (
+    <div>SideBarItemList</div>
+  )
+}
