bf401683d137512df798e4d63169ad31e389cbb9
phamhaduy95 <ph.duy113@gmail.com>
2022-08-10 15:53:05 +0700

    add App.styled.ts to contain style

---
+++ apps/test-lib/src/app/App.styled.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+
+export const StyledApp = styled("div")`
+    *,*::before,*::after {
+        box-sizing: border-box;
+    };
+    margin:1rem auto;
+    width:max-content;
+`;
