0e0a76a51570060d3bd86cdf831fcbb0d20cf9e1
phamhaduy95 <ph.duy113@gmail.com>
2022-08-27 13:48:35 +0700

    make extractTimeFromDate now accept nullable Date

--- libs/my-component/src/lib/utils/dateTime.ts
+++ libs/my-component/src/lib/utils/dateTime.ts
@@ -1,4 +1,5 @@
-export function extractTimeFromDate(date: Date) {
+export function extractTimeFromDate(date: Date|null) {
+    if (date === null) return {hour:0,minute:0,second:0}
     const hour = date.getHours();
     const second = date.getSeconds();
     const minute = date.getMinutes();
