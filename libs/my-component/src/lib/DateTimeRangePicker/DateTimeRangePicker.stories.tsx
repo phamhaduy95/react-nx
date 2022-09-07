0c63f7448293dac81e5b841af1df98c8a143e322
phamhaduy95 <ph.duy113@gmail.com>
2022-09-07 21:41:05 +0700

    add story for DateTimeRangePicker

---
+++ libs/my-component/src/lib/DateTimeRangePicker/DateTimeRangePicker.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { DateTimeRangePicker } from './DateTimeRangePicker';
+
+export default {
+  component: DateTimeRangePicker,
+  title: 'my-component/DateTimeRangePicker',
+} as ComponentMeta<typeof DateTimeRangePicker>;
+
+const Template: ComponentStory<typeof DateTimeRangePicker> = (args) => (
+  <DateTimeRangePicker {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  className:"",
+};
