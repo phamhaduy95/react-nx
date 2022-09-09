df47628ba75bdff0e9ede8606ae08be9e7cd6446
phamhaduy95 <ph.duy113@gmail.com>
2022-09-09 15:35:52 +0700

    add Story fro DatePicker

---
+++ libs/my-component/src/lib/DatePicker/DatePicker.stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { DatePicker } from './DatePicker';
+
+export default {
+  component: DatePicker,
+  title: 'DatePicker',
+  argTypes: {
+    onSelect: { action: 'onSelect executed!' },
+    PanelComponent: { action: 'PanelComponent executed!' },
+  },
+} as ComponentMeta<typeof DatePicker>;
+
+const Template: ComponentStory<typeof DatePicker> = (args) => (
+  <DatePicker {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  dateFormat: '',
+  label: '',
+};
