c6b2c3a5e3cd77eb90bd789470534b72ee003c4c
phamhaduy95 <ph.duy113@gmail.com>
2022-09-09 16:18:36 +0700

    add story for DateRangePicker

---
+++ libs/my-component/src/lib/DateRangePicker/DateRangePicker.stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { DateRangePicker } from './DateRangePicker';
+
+export default {
+  component: DateRangePicker,
+  title: 'my-component/DateRangePicker',
+  argTypes: {
+    onSelect: { action: 'onSelect executed!' },
+  },
+} as ComponentMeta<typeof DateRangePicker>;
+
+const Template: ComponentStory<typeof DateRangePicker> = (args) => (
+  <DateRangePicker {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  className: '',
+  dateFormat: '',
+  label: '',
+};
