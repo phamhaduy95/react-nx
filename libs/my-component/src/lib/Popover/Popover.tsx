7b2c9fc5f6d0114873b05e8655e8b18b74309cd2
phamhaduy95 <ph.duy113@gmail.com>
2022-09-22 01:00:59 +0700

    add forwardRef for Popover

--- libs/my-component/src/lib/Popover/Popover.tsx
+++ libs/my-component/src/lib/Popover/Popover.tsx
@@ -1,17 +1,22 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useImperativeHandle } from 'react';
 
 import { useMovePopoverOnScroll } from './useMovePopoverOnScroll';
-import { Placement, Position, usePopoverPlacement } from './usePopoverPlacement';
+import {
+  Placement,
+  Position,
+  usePopoverPlacement,
+} from './usePopoverPlacement';
 import { useEffectSkipFirstRender } from '../utils/useEffectSkipFirstRender';
 import classNames from 'classnames';
 import { createPortal } from 'react-dom';
 import { ClickOutSideWatcher } from '../ClickOutsideWatcher';
 import './Popover.scss';
+import { forwardRef } from 'react';
 
 export type PopoverProps = {
   children: React.ReactNode;
   className?: string;
-  anchorRef?: React.MutableRefObject<HTMLElement | null>|null;
+  anchorRef?: React.MutableRefObject<HTMLElement | null> | null;
   positionOrigin?: Position;
   placement?: Placement;
   isOpen: boolean;
@@ -20,7 +25,7 @@ export type PopoverProps = {
   fixedOnScroll?: boolean;
 };
 
-type key = 'children' | 'isOpen' ;
+type key = 'children' | 'isOpen';
 
 const defaultProps: Required<Omit<PopoverProps, key>> = {
   className: '',
@@ -29,53 +34,58 @@ const defaultProps: Required<Omit<PopoverProps, key>> = {
   forceMount: false,
   fixedOnScroll: false,
   positionOrigin: { top: 0, left: 0 },
-  anchorRef:null,
+  anchorRef: null,
 };
 
-export function Popover(props: PopoverProps) {
-  const newProps = { ...defaultProps, ...props };
-  const {
-    children,
-    placement,
-    positionOrigin,
-    anchorRef,
-    isOpen: openSignal,
-    onOpen,
-    className,
-    forceMount,
-    fixedOnScroll,
-  } = newProps;
-  const [isOpen, setOpen] = useState(false);
-  const popupRef = useRef<HTMLDivElement>(null);
+export const Popover = forwardRef<HTMLElement, PopoverProps>(
+  (props, ref) => {
+    const newProps = { ...defaultProps, ...props };
+    const {
+      children,
+      placement,
+      positionOrigin,
+      anchorRef,
+      isOpen: openSignal,
+      onOpen,
+      className,
+      forceMount,
+      fixedOnScroll,
+    } = newProps;
+    const [isOpen, setOpen] = useState(false);
+    const popupRef = useRef<HTMLDivElement>(null);
+    useImperativeHandle(ref, () => popupRef.current as HTMLElement, [
+      popupRef.current,
+    ]);
 
-  useEffectSkipFirstRender(() => {
-    onOpen(isOpen);
-  }, [isOpen]);
+    useEffectSkipFirstRender(() => {
+      onOpen(isOpen);
+    }, [isOpen]);
 
-  useEffect(() => {
-    setOpen(openSignal);
-  }, [openSignal]);
+    useEffect(() => {
+      setOpen(openSignal);
+    }, [openSignal]);
 
-  usePopoverPlacement(popupRef, anchorRef, placement, positionOrigin, isOpen);
-  useMovePopoverOnScroll(popupRef, isOpen, fixedOnScroll);
+    usePopoverPlacement(popupRef, anchorRef, placement, positionOrigin, isOpen);
+    useMovePopoverOnScroll(popupRef, isOpen, fixedOnScroll);
 
-  const rootClassName = classNames('Popover', className, {
-    ['is-open']: isOpen,
-  });
-  const handleClickOutSide = () => {
-    onOpen(false);
-    setOpen(false);
-  };
-  return (
-    <PortalProvider forceMount={forceMount} isOpen={isOpen}>
-      <ClickOutSideWatcher ref={popupRef} onClickOutSide={handleClickOutSide}>
-        <div className={rootClassName} ref={popupRef}>
-          {children}
-        </div>
-      </ClickOutSideWatcher>
-    </PortalProvider>
-  );
-}
+    const rootClassName = classNames('Popover', className, {
+      ['is-open']: isOpen,
+    });
+    const handleClickOutSide = () => {
+      onOpen(false);
+      setOpen(false);
+    };
+    return (
+      <PortalProvider forceMount={forceMount} isOpen={isOpen}>
+        <ClickOutSideWatcher ref={popupRef} onClickOutSide={handleClickOutSide}>
+          <div className={rootClassName} ref={popupRef}>
+            {children}
+          </div>
+        </ClickOutSideWatcher>
+      </PortalProvider>
+    );
+  }
+);
 
 type PortalProviderProps = {
   forceMount: boolean;
