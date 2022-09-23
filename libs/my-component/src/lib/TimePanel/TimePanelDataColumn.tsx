06d935a13d973eb2128236abab711c4cc1e6144b
phamhaduy95 <ph.duy113@gmail.com>
2022-09-23 14:53:34 +0700

    improve performance for TimePanel in my-component.
    - use memo for TimePanelDataColumn

--- libs/my-component/src/lib/TimePanel/TimePanelDataColumn.tsx
+++ libs/my-component/src/lib/TimePanel/TimePanelDataColumn.tsx
@@ -2,24 +2,28 @@ import {
   ScrollableDataColumn,
   ScrollableDataColumnProps,
 } from '@phduylib/my-component';
+import { memo } from 'react';
 
 type Props = {
   className: string;
   dataSet: ScrollableDataColumnProps['dataSet'];
   numberShowedItem: number;
   onSelect: ScrollableDataColumnProps['onSelect'];
-  value: number;
+  value: number|undefined;
 };
 
-export default function TimePanelDataColumn(props: Props) {
+export const TimePanelDataColumn = memo((props: Props)=> {
   const { className, dataSet, numberShowedItem, onSelect, value } = props;
+  let newValue = value === undefined?null:value;
+
+  console.log(className," rendered")
   return (
     <ScrollableDataColumn
       className={className}
       dataSet={dataSet}
       numberShowedItem={numberShowedItem}
       onSelect={onSelect}
-      initialSelected={value}
+      value={newValue}
     />
   );
-}
+});
