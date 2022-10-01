9b196120d0717625328621750661ba797e7dfb1a
phamhaduy95 <ph.duy113@gmail.com>
2022-10-01 11:33:57 +0700

    update DateTimeRangePicker in my-component
    - add className prop

--- libs/my-component/src/lib/DateTimeRangePicker/DateTimeRangePicker.tsx
+++ libs/my-component/src/lib/DateTimeRangePicker/DateTimeRangePicker.tsx
@@ -1,18 +1,29 @@
 import { DateTimeRangeInput } from './DateTimeRangeInput';
-import { DateTimeRangePickerStoreProvider, useDateTimeRangePickerStore } from './DateTimeRangePickerStoreProvider';
+import {
+  DateTimeRangePickerStoreProvider,
+  useDateTimeRangePickerStore,
+} from './DateTimeRangePickerStoreProvider';
+import classNames from 'classnames';
 export type DateTimeRangePickerProps = {
   className?: string;
 };
 
+const defaultProps:Required<DateTimeRangePickerProps> = {
+  className:"",
+}
+
+
 export function DateTimeRangePicker(props: DateTimeRangePickerProps) {
   return (
     <DateTimeRangePickerStoreProvider>
-      <WrappedComponent />
+      <WrappedComponent {...props}/>
     </DateTimeRangePickerStoreProvider>
   );
 }
 
 function WrappedComponent(props: DateTimeRangePickerProps) {
+  const newProps = {...defaultProps,...props};
+  const { className } = newProps;
   const action = useDateTimeRangePickerStore((state) => state.action);
   const handleStartDateSelect = (date: Date | null) => {
     action.selectStartDate(date);
@@ -21,16 +32,18 @@ function WrappedComponent(props: DateTimeRangePickerProps) {
     action.selectEndDate(date);
   };
 
+  const rootClassName = classNames('DateTimeRangePicker', className);
+
   return (
-    <div className="DateTimeRangePicker">
+    <div className={rootClassName}>
       <DateTimeRangeInput
         mode="selectStart"
-        label=""
+        label="StartTime"
         onDateSelect={handleStartDateSelect}
       />
       <DateTimeRangeInput
         mode="selectEnd"
-        label=""
+        label="EndTime"
         onDateSelect={handleEndDateSelect}
       />
     </div>
