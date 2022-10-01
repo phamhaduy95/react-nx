c41380b54ab2cecda79545812f3634d22205e758
phamhaduy95 <ph.duy113@gmail.com>
2022-10-01 09:51:46 +0700

    add useEffectSkipFirstRendering in todo-app

---
+++ apps/todo-app/src/utils/hooks.ts
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+/** this hook provide safer way to invoke any event callback function since its wont trigger the callback when the React component is first rendered */ 
+export function useEffectSkipFirstRender(callback:Function,dependencyArray:NonNullable<any>[]){
+    const [firstRender,setFirstRender] = useState(false);
+    useEffect(()=>{
+        setFirstRender(true);
+    },[])
+    useEffect(()=>{
+        if (!firstRender) return;
+        callback();  
+    },[...dependencyArray])
+}
