779187f290196ec8d74ba362f6dadad6ce8d64fd
phamhaduy95 <ph.duy113@gmail.com>
2022-10-13 13:33:34 +0700

    remove redux slice for MonthSchedule in todo-app

--- apps/todo-app/src/components/MonthSchedule/redux/types.ts
+++
@@ -1,9 +0,0 @@
-import { ReduxTaskData } from "apps/todo-app/src/redux/types";
-
-
-type DateStr = string;
-
-export type ReduxMonthScheduleState = {
-  month: DateStr;
-  tasks: ReduxTaskData[];
-};
