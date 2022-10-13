c2fbfc675737abe9c16aadc0a717331c0a14d10b
phamhaduy95 <ph.duy113@gmail.com>
2022-10-13 13:33:07 +0700

    remove slice for DaySchedule in todo-app

--- apps/todo-app/src/components/DaySchedule/redux/types.ts
+++
@@ -1,9 +0,0 @@
-import { ReduxTaskData } from "apps/todo-app/src/redux/types";
-
-
-type DateStr = string;
-
-export type ReduxDayScheduleState = {
-  date: DateStr;
-  tasksList: ReduxTaskData[];
-};
