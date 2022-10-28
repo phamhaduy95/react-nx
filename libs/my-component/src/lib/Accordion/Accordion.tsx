a4625fa7350e6e86b7903e275f2776e5ef37b983
phamhaduy95 <ph.duy113@gmail.com>
2022-10-29 01:05:25 +0700

    update Accordion in my-component
    - apply global style.
    - use classNames lib to add external class.

--- libs/my-component/src/lib/Accordion/Accordion.tsx
+++ libs/my-component/src/lib/Accordion/Accordion.tsx
@@ -2,6 +2,8 @@ import './Accordion.scss';
 import AccordionStoreProvider from './AccordionStoreProvider';
 import { useEffect } from 'react';
 import { useAccordionStore } from './AccordionStoreProvider';
+import GlobalStyleProvider from '../GlobalStyleProvider';
+import classNames from 'classnames';
 
 export type AccordionProps = {
   children: JSX.Element[] | JSX.Element;
@@ -16,22 +18,24 @@ const defaultProps: Required<AccordionProps> = {
 };
 
 export function Accordion(props: AccordionProps) {
-
   return (
-      <AccordionStoreProvider>
-        <WrappedAccordion {...props}/>
-      </AccordionStoreProvider>
+    <AccordionStoreProvider>
+      <WrappedAccordion {...props} />
+    </AccordionStoreProvider>
   );
 }
-function WrappedAccordion(props:AccordionProps) {
+function WrappedAccordion(props: AccordionProps) {
   const newPros = { ...defaultProps, ...props };
   const { className, children, isAlwaysOpen } = newPros;
-  const action = useAccordionStore((state)=>state.action);
-  useEffect(()=>{
-      action.changeSettings({isAlwaysOpen})
-  },[isAlwaysOpen])
+  const action = useAccordionStore((state) => state.action);
+  const rootClassName = classNames('Accordion', className);
+  useEffect(() => {
+    action.changeSettings({ isAlwaysOpen });
+  }, [isAlwaysOpen]);
 
-return (
-  <div className={`Accordion ${className}`}>{children}</div>
-)
+  return (
+    <GlobalStyleProvider>
+      <div className={rootClassName}>{children}</div>
+    </GlobalStyleProvider>
+  );
 }
