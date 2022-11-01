76f3a5d766cd43803a1671cddfe1d6af9e95f2c3
phamhaduy95 <ph.duy113@gmail.com>
2022-11-01 11:01:28 +0700

    add AddCategoryModal in todo-app.

--- apps/todo-app/src/components/AddCategoryModal/AddCategoryModal.tsx
+++ apps/todo-app/src/components/AddCategoryModal/AddCategoryModal.tsx
@@ -4,10 +4,13 @@ import {
   ModalBody,
   ModalProps,
   TextField,
+  ModalFooter,
+  Button,
+  IconButton,
 } from '@phduylib/my-component';
-import React from 'react';
 import { useAppAction, useAppDispatch, useAppSelector } from '../../redux';
 import CloseIcon from '@mui/icons-material/Close';
+import './AddCategoryModal.scss';
 
 export function AddCategoryModal() {
   const action = useAppAction();
@@ -18,22 +21,60 @@ export function AddCategoryModal() {
     dispatch(action.AddCategoryModal.toggleOpen(isOpen));
   };
 
+  const closeModalSignal = () => {
+    dispatch(action.AddCategoryModal.toggleOpen(false));
+  };
+
+  const handleClear = () => {};
+
+  const handleSubmit = () => {};
+
   return (
     <Modal
       className="AddCategoryModal"
       isOpen={isOpen}
       onToggle={handleModalOpen}
-      closeIcon={<CloseIcon />}
     >
-      <ModalHeader>
-        <p className="AddCategoryModal__Title">Add New Category</p>
+      <ModalHeader className="AddCategoryModal__Header">
+        <span className="AddCategoryModal__Title">Add Category</span>
+
+        <IconButton
+          className="AddCategoryModal__CloseButton"
+          onClick={closeModalSignal}
+          variant="secondary"
+        >
+          <CloseIcon />
+        </IconButton>
       </ModalHeader>
       <ModalBody className="AddCategoryModal__Body">
         <TextField
           className="AddCategoryModal__NameInput"
           label="categoryName"
         />
+        <div className="AddCategoryModal__ColorPicker">
+          <label className="AddCategoryModal__Label">color</label>
+          <input className="AddCategoryModal__ColorInput" type="color" />
+        </div>
+        <div className="AddCategoryModal__Description">
+          <label className="AddCategoryModal__Label">description</label>
+          <textarea className="AddCategoryModal__TextArea" />
+        </div>
       </ModalBody>
+      <ModalFooter className="AddCategoryModal__Footer">
+        <Button
+          className="AddCategoryModal__SubmitButton"
+          onClick={handleSubmit}
+        >
+          Add
+        </Button>
+        <Button
+          className="AddCategoryModal__CancelButton"
+          onClick={handleClear}
+          type="outlined"
+        >
+          Clear
+        </Button>
+      </ModalFooter>
     </Modal>
   );
 }
