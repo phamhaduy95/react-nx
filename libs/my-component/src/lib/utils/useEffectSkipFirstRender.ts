3dc66a7dd0826cea4bd1289eefb4f20a55dd4b1c
phamhaduy95 <ph.duy113@gmail.com>
2022-11-13 16:34:25 +0700

    update util in my-component
    useEffectSkipFirstRendering now accept clean up code just like useEffect does

--- libs/my-component/src/lib/utils/useEffectSkipFirstRender.ts
+++ libs/my-component/src/lib/utils/useEffectSkipFirstRender.ts
@@ -1,13 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,EffectCallback } from "react";
+
 /** this hook provide safer way to invoke any event callback function since its wont trigger the callback when the React component is first rendered */ 
-export function useEffectSkipFirstRender(callback:Function,dependencyArray:NonNullable<any>[]){
+
+
+
+export function useEffectSkipFirstRender(callback:EffectCallback,dependencyArray:NonNullable<any>[]){
     const [firstRender,setFirstRender] = useState(false);
     useEffect(()=>{
         setFirstRender(true);
     },[])
     useEffect(()=>{
         if (!firstRender) return;
-        callback();  
+        const cleanup = callback();
+        return cleanup;
     },[...dependencyArray])
 }
 
