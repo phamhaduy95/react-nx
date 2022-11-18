abc91cadcd032b82f9b1e25e74f5455cd5c5ecc0
phamhaduy95 <ph.duy113@gmail.com>
2022-11-18 14:44:53 +0700

    initial portfolio project

---
+++ apps/portfolio/src/app/App.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './App.module.scss';
+
+export function App() {
+  return (
+    <>
+      <div className={styles['MyButton']}>OK</div>
+      <div className={styles['MyButton']}>OK</div>
+    </>
+  );
+}
+
+export default App;
