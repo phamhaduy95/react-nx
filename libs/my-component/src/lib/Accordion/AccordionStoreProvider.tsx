ef767a509057b5a38d6d8709964b0582e1c7a6a2
phamhaduy95 <ph.duy113@gmail.com>
2023-10-28 20:43:35 +0700

    update Item

--- libs/my-component/src/lib/Accordion/AccordionStoreProvider.tsx
+++ libs/my-component/src/lib/Accordion/AccordionStoreProvider.tsx
@@ -2,23 +2,20 @@ import React, { createContext, useContext, useMemo } from 'react';
 import { createStore, StoreApi, useStore } from 'zustand';
 import shallow from 'zustand/shallow';
 
-
 type AccordionState = {
-  itemList: {id:string,isOpen:boolean}[];
+  itemList: { id: string; isOpen: boolean }[];
   settings: {
-    isAlwaysOpen:boolean;
-  }
+    isAlwaysOpen: boolean;
+  };
   action: {
-    toggleItem: (item:{id:string,isOpen:boolean}) => void;
-    subscribe: (id:string)=>void;
-    unsubscribe: (id:string)=>void;
-    changeSettings:(setting:AccordionState["settings"])=>void;
+    toggleItem: (item: { id: string; isOpen: boolean }) => void;
+    subscribe: (id: string) => void;
+    unsubscribe: (id: string) => void;
+    changeSettings: (setting: AccordionState['settings']) => void;
   };
 };
 
-type StoreContextValue = 
-  StoreApi<AccordionState>
- | null;
+type StoreContextValue = StoreApi<AccordionState> | null;
 
 const StoreContext = createContext<StoreContextValue>(null);
 
@@ -30,50 +27,50 @@ export default function AccordionStoreProvider(props: Props) {
   const { children } = props;
   const store = useMemo(() => {
     return createStore<AccordionState>((set) => ({
-      itemList:[],
-      settings:{
-        isAlwaysOpen:false,
+      itemList: [],
+      settings: {
+        isAlwaysOpen: false,
       },
       action: {
         toggleItem(item) {
-            set((state)=>{
-                const {id,isOpen} = item;
-                const {isAlwaysOpen} = state.settings;
-                const itemList = [...state.itemList];
-                if (!isAlwaysOpen && isOpen ) {
-                  closeAllButOpenOneItem(itemList,id);
-                  return {itemList:itemList}
-                };
-                toggleOneItemSeparately(itemList,id,isOpen);
-                return {itemList:itemList}
-            })
+          set((state) => {
+            const { id, isOpen } = item;
+            const { isAlwaysOpen } = state.settings;
+            const itemList = [...state.itemList];
+            if (!isAlwaysOpen && isOpen) {
+              closeAllButOpenOneItem(itemList, id);
+              return { itemList: itemList };
+            }
+            toggleOneItemSeparately(itemList, id, isOpen);
+            return { itemList: itemList };
+          });
         },
         changeSettings(setting) {
-              set((state)=>{
-                  if (shallow(setting,state.settings)) return {};
-                  return {settings:setting}
-              });
+          set((state) => {
+            if (shallow(setting, state.settings)) return {};
+            return { settings: setting };
+          });
         },
         subscribe(id) {
-          set((state)=>{
-              const itemList = [...state.itemList];
-              itemList.push({id:id,isOpen:false});
-              return {itemList:itemList}
+          set((state) => {
+            const itemList = [...state.itemList];
+            itemList.push({ id: id, isOpen: false });
+            return { itemList: itemList };
           });
         },
         unsubscribe(id) {
-          set((state)=>{
+          set((state) => {
             const itemList = [...state.itemList];
-              itemList.filter((item)=>item.id !== id);
-              return {itemList:itemList}
-        });
+            itemList.filter((item) => item.id !== id);
+            return { itemList: itemList };
+          });
         },
       },
     }));
   }, []);
 
   return (
-    <StoreContext.Provider value={ store }>{children}</StoreContext.Provider>
+    <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
   );
 }
 
@@ -81,26 +78,33 @@ export function useAccordionStore<U>(
   selector: (state: AccordionState) => U,
   equalFunc?: (a: U, b: U) => boolean
 ): U {
-    const value = useContext(StoreContext);
-    if (value === null) throw new Error("Accordion Store context is null");
-    const store = value;
-    return useStore(store,selector,equalFunc);
+  const value = useContext(StoreContext);
+  if (value === null) throw new Error('Accordion Store context is null');
+  const store = value;
+  return useStore(store, selector, equalFunc);
 }
 
-function closeAllButOpenOneItem(itemList:AccordionState["itemList"],openItemId:string){
-    for (let item of itemList){
-        if (item.id === openItemId) {
-          item.isOpen = true
-         continue
-        }
-        item.isOpen = false;
-    }
-}
-
-function toggleOneItemSeparately(itemList:AccordionState["itemList"],openItemId:string,isOpen:boolean){
-  for (let item of itemList){
+function closeAllButOpenOneItem(
+  itemList: AccordionState['itemList'],
+  openItemId: string
+) {
+  for (const item of itemList) {
     if (item.id === openItemId) {
-      item.isOpen = isOpen
+      item.isOpen = true;
+      continue;
     }
+    item.isOpen = false;
+  }
 }
+
+function toggleOneItemSeparately(
+  itemList: AccordionState['itemList'],
+  openItemId: string,
+  isOpen: boolean
+) {
+  for (const item of itemList) {
+    if (item.id === openItemId) {
+      item.isOpen = isOpen;
+    }
+  }
 }
--- libs/my-component/src/lib/BreadCrumbs/BreadCrumbItem.tsx
+++ libs/my-component/src/lib/BreadCrumbs/BreadCrumbItem.tsx
@@ -2,61 +2,61 @@ import React, { memo } from 'react';
 import { ensureElementsListAsArray } from '../utils/ReactElementProcessor';
 import classNames from 'classnames';
 type BreadCrumbsItemProps = {
-  children: React.ReactNode;
-  value: string;
-  className?: string;
-  menu?: string[];
-  disabled?: boolean;
+    children: React.ReactNode;
+    value: string;
+    className?: string;
+    menu?: string[];
+    disabled?: boolean;
 };
 
 const defaultProps: Omit<
-  Required<BreadCrumbsItemProps>,
-  'children' | 'className'
+    Required<BreadCrumbsItemProps>,
+    'children' | 'className'
 > = {
-  value: '',
-  menu: [],
-  disabled: false,
+    value: '',
+    menu: [],
+    disabled: false,
 };
 
 export function BreadCrumbsItem(props: BreadCrumbsItemProps) {
-  return <></>;
+    return <></>;
 }
 
 export function giveIndexForBreadCrumbsItem(
-  items: JSX.Element[] | JSX.Element
+    items: JSX.Element[] | JSX.Element
 ) {
-  const arrayItems = ensureElementsListAsArray(items);
-  const returnList: JSX.Element[] = [];
-  let i = 0;
-  for (let item of arrayItems) {
-    if (item.type.name !== BreadCrumbsItem.name) continue;
-    const props = item.props as BreadCrumbsItemProps;
-    const isActive = i === arrayItems.length - 1;
-    const indexedItem = (
-      <IndexedBreadCrumbItem
-        {...props}
-        key={`breadcrumbsItem-${i}`}
-        isActive={isActive}
-      />
-    );
-    i++;
-    returnList.push(indexedItem);
-  }
-  return returnList;
+    const arrayItems = ensureElementsListAsArray(items);
+    const returnList: JSX.Element[] = [];
+    let i = 0;
+    for (const item of arrayItems) {
+        if (item.type.name !== BreadCrumbsItem.name) continue;
+        const props = item.props as BreadCrumbsItemProps;
+        const isActive = i === arrayItems.length - 1;
+        const indexedItem = (
+            <IndexedBreadCrumbItem
+                {...props}
+                key={`breadcrumbsItem-${i}`}
+                isActive={isActive}
+            />
+        );
+        i++;
+        returnList.push(indexedItem);
+    }
+    return returnList;
 }
 
 type IndexedItemProps = BreadCrumbsItemProps & { isActive: boolean };
 
 export const IndexedBreadCrumbItem = memo((props: IndexedItemProps) => {
-  const newProps = { ...defaultProps, ...props };
+    const newProps = { ...defaultProps, ...props };
 
-  const { children, value, menu, disabled, className, isActive } = newProps;
-  const rootClassName = classNames('BreadCrumbs__Item', className, {
-    disabled: disabled,
-    ['is-active']: isActive,
-  });
+    const { children, value, menu, disabled, className, isActive } = newProps;
+    const rootClassName = classNames('BreadCrumbs__Item', className, {
+        disabled: disabled,
+        ['is-active']: isActive,
+    });
 
-  return <span className={rootClassName}>{children}</span>;
+    return <span className={rootClassName}>{children}</span>;
 });
 
 export default BreadCrumbsItem;
